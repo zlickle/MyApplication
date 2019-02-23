a3bd91c127c1ec0dff2a7ca7693fc80fc5e2c1fb
millionape <kittaporn.jp@gmail.com>
2019-02-23 15:14:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import org.json.JSONArray
+import java.io.IOException
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        run("http://119.59.116.171:8080/values")
+
+    }
+
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) {
+                var res = response.body()?.string()
+                val json = JSONArray(res)
+                val id = json.getJSONObject(0).getString("id")
+                txt_id.setText(id)
+            }
+
+        })
+    }
+
+}
+
+
+
